9a4fd1db17021fa827f9b3bb8a0b61703acf8703
Breno Guimaraes <devbreno4@gmail.com>
2026-03-16 15:05:37 -0300

    function-types

---
+++ src/function-types/function-types.ts
@@ -0,0 +1,24 @@
+
+ type MapStringsCallback = (item:string) =>string;
+
+function mapStrings(array: string[],callbackfn: MapStringsCallback): string[]{
+    const newArray: string[] = [];
+   
+
+    for(let i =0; i<array.length; i++)
+    {
+        newArray.push(callbackfn(array[i]));
+    }
+
+    return newArray; 
+}
+
+const abc = ['a', 'b', 'c']; 
+const abcMapped = mapStrings(abc, (item: any)=> item.toUpperCase()); 
+
+console.log(abcMapped);
+
+// code with type function 
+
+
+
