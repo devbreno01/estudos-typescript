b387b3ebd9217b5683b2b383cfa3caf2ae9bed81
Breno Guimarães <devbreno4@gmail.com>
2026-03-18 07:56:37 -0300

    config webpack part 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-import path from "node:path";
-import { fileURLToPath } from 'url';
+const path = require('path');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
- export default {
-   mode: 'development',
-   entry: './src/webpack/index.ts',
-   devtool: 'inline-source-map',
-   module: {
-     rules: [
-       {
-         test: /\.tsx?$/,
-         use: 'ts-loader',
-         exclude: /node_modules/,
-       },
-     ],
-   },
-   resolve: {
-     extensions: [ '.tsx', '.ts', '.js' ],
-   },
-   output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'dist','assets','js'),
-   },
-   devtool: 'source-map',
- };
+module.exports = {
+  mode: 'development',
+  entry: './src/webpack/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.frontend.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  devtool: 'source-map',
+};
